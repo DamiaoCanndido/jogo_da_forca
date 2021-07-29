25ae8dbc99af100640332198f1b94623e599fa41
DamiaoCanndido <tower_gamer@hotmail.com>
2021-07-29 15:24:56 -0300

    Criando forca e varaiáveis.

---
+++ main.py
@@ -0,0 +1,20 @@
+cabeca =  ' '#'O'
+corpo = ' '#'|'
+braco_esq = ' '#'/'
+braco_dir = ' '#'\\'
+perna_esq = ' '#'/'
+perna_dir = ' '#'\\'
+
+forca = f"########\n##     |\n##     {cabeca}\n##    {braco_esq}{corpo}{braco_dir}\n##     {corpo}\n##    {perna_esq} {perna_dir}\n##"
+
+vidas = 6
+
+pecados = ['soberba', 'avareza', 'inveja', 'ira', 'luxuria', 'gula', 'preguica']
+
+def sorteio(lista: list[str]):
+    pass
+
+def jogar(letra: str):
+    pass
+
+print(forca)
